dcff88bb4783b67ac94497aeba8bc3a14b296f3b
Francis Dierick <fdierick@Franciss-MacBook-Pro.local>
2017-08-29 06:23:01 +0200

    Modded for test run

--- code/ico/migrations/2_deploy_contracts.js
+++ code/ico/migrations/2_deploy_contracts.js
@@ -3,12 +3,12 @@ var TeamCoinSale = artifacts.require("./TeamCoinSale.sol");
 
 module.exports = function(deployer) {
   //deployer.deploy(TeamCoin, {gas: 1000000}); // Handled by TeamCoin migration
-  
+ /* 
   const startBlock = web3.eth.blockNumber + 2 // blockchain block number where the crowdsale will commence. Here I just taking the current block that the contract and setting that the crowdsale starts two block after
   const endBlock = startBlock + 100  // blockchain block number where it will end. 300 is little over an hour.
   const rate = new web3.BigNumber(1000) // rate of ether to Gustavo Coin in wei
   const wallet = web3.eth.accounts[0] // the address that will hold the fund. Recommended to use a multisig one for security.
-
-  deployer.deploy(TeamCoinSale, startBlock, endBlock, rate, wallet, {gas: 1000000, gasPrice: 100000000000});
+*/
+  deployer.deploy(TeamCoinSale);
 
 };
--- code/ico/truffle.js
+++ code/ico/truffle.js
@@ -1,32 +1,3 @@
-var bip39 = require("bip39");
-var hdkey = require('ethereumjs-wallet/hdkey');
-var ProviderEngine = require("web3-provider-engine");
-var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
-var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
-var FilterSubprovider = require('web3-provider-engine/subproviders/filters.js')
-var Web3 = require("web3");
-
-// Get our mnemonic and create an hdwallet
-var mnemonic = "fall summer baby potato annual during force horror orbit chimney mercy fancy begin coach excess";
-var hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
-
-// Get the first account using the standard hd path.
-var wallet_hdpath = "m/44'/60'/0'/0/";
-var wallet = hdwallet.derivePath(wallet_hdpath + "0").getWallet();
-var address = "0x" + wallet.getAddress().toString("hex");
-
-var providerUrl = "https://mainnet.infura.io/RhuuIFkweu2iRs3EkaAB "; // 
-var engine = new ProviderEngine();
-engine.addProvider(new WalletSubprovider(wallet, {}));
-engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(providerUrl)));
-engine.addProvider(new FilterSubprovider())
-
-engine.on('error', function(err) {
-    console.error(err.stack)
-});
-
-engine.start(); // Required by the provider engine.
-
 module.exports = {
   networks: {
     development: {
@@ -37,8 +8,8 @@ module.exports = {
     },
     live: {
       network_id: 1,    // Official ropsten network id
-      provider: engine, // Use our custom provider
-      from: address,     // Use the address we derived
+      //provider: engine, // Use our custom provider
+      //from: address,     // Use the address we derived
       gas: 1000000,
       gasPrice: 100000000000
     }
