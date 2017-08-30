bbc43b9c05028c7f11b956b2f6272cc0d899f1b1
Francis Dierick <francis@dierick.co>
2017-08-30 07:15:37 +0200

    Hardcoded token recovery address

--- code/ico/contracts/TeamCoin.sol
+++ code/ico/contracts/TeamCoin.sol
@@ -11,8 +11,7 @@ contract TeamCoin is Ownable, Destructible, Contactable, MintableToken {
 
 	string public name = 'TeamCoin';
 	string public symbol = 'TMC';
-	string public terms = 'ipfs:QmPtyYSXbPedgnijREBgHnDs6Jt1ssQk8NMgHuTchoeKZM';
-	string public terms_md5 = '1aefd0716c03cfe5803598e6d27d918b';
+	string public info = 'ipfs:QmPtyYSXbPedgnijREBgHnDs6Jt1ssQk8NMgHuTchoeKZM';
 	uint public decimals = 18;
 	uint public INITIAL_SUPPLY = 1201000000000000000000000;
 	address public owner;
@@ -27,15 +26,11 @@ contract TeamCoin is Ownable, Destructible, Contactable, MintableToken {
   	}
 
 	function TeamCoin() {
-		
-		// Locking the contract disables the transfer functions.
 		locked = true;
-
-		// Set according to this formula for live: last_block_number + ((future_time - time_now) / block_time)
+  		owner = 0xDFDaCCAE897B008F814225dc67AaCa3aB41250A7;
   		totalSupply = INITIAL_SUPPLY;
-  		balances[msg.sender] = INITIAL_SUPPLY; // set to wallet
-  		owner = msg.sender;
-  		contactInformation = "web: http://www.teamco.in email: francis@dierick.co twitter: @macbookjockey";
+  		balances[owner] = INITIAL_SUPPLY;
+  		contactInformation = "http://www.teamco.in";
 	}
 
 	function unlock() onlyOwner {
@@ -52,10 +47,6 @@ contract TeamCoin is Ownable, Destructible, Contactable, MintableToken {
 
 	function transferFrom(address _from, address _to, uint256 _value) onlyUnlocked returns (bool) {
 		var _allowance = allowed[_from][msg.sender];
-
-		// Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
-		// require (_value <= _allowance);
-
 		balances[_to] = balances[_to].add(_value);
 		balances[_from] = balances[_from].sub(_value);
 		allowed[_from][msg.sender] = _allowance.sub(_value);
